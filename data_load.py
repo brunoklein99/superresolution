a11db7d48c280209e490af2ddcb45c1b3469abe0
Bruno Klein Salvalaio <brunoklein99@gmail.com>
2018-03-18 16:43:26 -0300

    changes data dir to parent dir

--- data_load.py
+++ data_load.py
@@ -5,8 +5,8 @@ from sklearn.model_selection import train_test_split
 
 
 def load_data():
-    files = listdir('images')
-    files = [join('images', x) for x in files]
+    files = listdir('../images')
+    files = [join('../images', x) for x in files]
     train, valid_test = train_test_split(files, test_size=0.1)
     valid, test = train_test_split(valid_test, test_size=0.5)
     return train, valid, test
